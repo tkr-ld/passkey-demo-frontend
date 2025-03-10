517e0dac3e00cd7fc6b3f063d2f08c8b524637ad
tkr-ld <sonobetakuro@team-lab.com>
2025-03-11 00:03:03 +0900

    navigator.credentials.create()が動くようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import styles from "./App.module.scss";
+import { Button, ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const onClick = async () => {
+    console.log("onClick");
+    const credentials = await navigator.credentials.create({
+      publicKey: {
+        challenge: new Uint8Array(32),
+        rp: { id: "localhost", name: "passkey example" },
+        user: { id: new Uint8Array(32), name: "user", displayName: "User" },
+        pubKeyCredParams: [
+          { type: "public-key", alg: -8 },
+          { type: "public-key", alg: -7 },
+          { type: "public-key", alg: -257 },
+        ],
+        authenticatorSelection: { userVerification: "required" },
+        timeout: 60000,
+        attestation: "direct",
+      },
+    });
+    console.log(credentials)
+    // TODO send credentials to server
+  };
 
   return (
-    <>
-      <div>
+    <ChakraProvider value={defaultSystem}>
+      <Button onClick={onClick}>パスキー新規登録</Button>
+      {/* <div>
         <div className={styles.test}>test</div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,9 +49,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </p> */}
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
